ffd3b53d3f19d7ec3cf39e89773014403f5badd1
Lokesh Rathi <38494676+Lokeshrathi@users.noreply.github.com>
2020-05-02 19:14:27 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,16 @@
+from cv2 import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier("/home/lokesh/anaconda3/envs/my_env/Detect/haarcascade_frontalface_default.xml")
+# Read the input image
+img = cv2.imread("/home/lokesh/anaconda3/envs/my_env/Detect/WhatsApp Image 2020-04-28 at 18.18.35 (copy).jpeg")
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 3)
+# Display the output
+cv2.imshow('img', img)
+cv2.waitKey()
